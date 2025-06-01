9ae90217283801e485f2004a4cd00cabb9e070c7
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-06-01 19:01:49 +0700

    first init

---
+++ README.md
@@ -0,0 +1,3 @@
+# clean_arsitektur
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/clean_arsitektur/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clean_arsitektur
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
